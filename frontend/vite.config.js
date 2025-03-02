71ebefd11a63047a852ae04c6ba059f982f98b4a
whalethreewhy <wail3y1125@gmail.com>
2025-03-02 05:44:35 +0000

    Try hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), tailwindcss()],
     server: {
       port: 3000,
+      host: '0.0.0.0',
+      allowedHosts: true,
     },
   }
 })
