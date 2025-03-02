017351ef4ae6b3f9958ecc236cded8227dd41bbb
Adrian Yip <adrianphyip@gmail.com>
2025-03-02 09:52:50 +0000

    Changed Icon to Correct Logo, changed features writing in home page. Got rid of testimonial file as it is unused

--- frontend/src/partials/FeaturesHome.jsx
+++ frontend/src/partials/FeaturesHome.jsx
@@ -15,12 +15,13 @@ function FeaturesHome() {
                     <p className="text-gray-600 mt-2">Instantly connect with an accountability partner.</p>
                 </div>
                 <div className="p-6 bg-white rounded-lg shadow-lg text-center">
-                    <h3 className="text-2xl font-semibold">AI Check-ins</h3>
-                    <p className="text-gray-600 mt-2">Stay on track with periodic AI reminders.</p>
+                    <h3 className="text-2xl font-semibold">Timed Check-ins</h3>
+                    <p className="text-gray-600 mt-2">Stay on track with periodic reminders.</p>
                 </div>
                 <div className="p-6 bg-white rounded-lg shadow-lg text-center">
-                    <h3 className="text-2xl font-semibold">AI-Generated Timelines</h3>
-                    <p className="text-gray-600 mt-2">Get a detailed breakdown of your work sessions.</p>
+                    <h3 className="text-2xl font-semibold">Gamified AI Work-Insights</h3>
+                    <p className="text-gray-600 mt-2">Track and compare your productivity in real-time.
+                    </p>
                 </div>
             </div>
         </div>
